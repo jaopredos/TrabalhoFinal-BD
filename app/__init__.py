2516785e009180b923f1cbce2b60b1cd2af5aab3
Cleiver Júnior <jr.cleiver@gmail.com>
2025-06-27 03:34:42 -0300

    Adicionando secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__, template_folder='templates') 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'chave-secreta-flask')
 login_manager = LoginManager(app)
+
 
 from app import route
 from app import funcionario
